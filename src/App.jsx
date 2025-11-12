9c72fb50cff1a180fcb461149ff176e2b0da6b30
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-12 14:44:16 +0100

    add height and py

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 function App() {
   
   const [attrici, setAttrici] = useState([])
+  const [attori, setAttori] = useState([])
   const url = ' https://lanciweb.github.io/demo/api/actresses/'
 
   useEffect(ottenereAttrici, [])
@@ -26,13 +27,13 @@ function App() {
     <ul className='list-unstyled row d-flex'>
     {
       attrici.map(attrice => (
-        <li key={attrice.id} className='col-4' >
+        <li key={attrice.id} className='col-4 py-3' >
           <div className='card'>
             <div className='card-top'>
           <img  src={attrice.image} className='' alt={attrice.name} />
             </div>
             <div className='card-bottom'>
-          <p>Nome: {attrice.name}</p>
+          <p><strong>Nome:</strong> {attrice.name}</p>
           <p>Anno di nascita: {attrice.birth_year}</p>
           <p>Biografia: {attrice.biography}</p>
           <p>Premi: {attrice.awards}</p>
