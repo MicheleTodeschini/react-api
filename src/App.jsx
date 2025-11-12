aa07a22469a6d6e5a23c8944e0388ae40d7ad46b
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-12 12:54:37 +0100

    add description

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios'
 
@@ -7,10 +7,10 @@ function App() {
   const [attrici, setAttrici] = useState([])
   const url = ' https://lanciweb.github.io/demo/api/actresses/'
 
-  useState(handleClick, [])
+  useEffect(ottenereAttrici, [])
 
 
-    function handleClick() {
+    function ottenereAttrici() {
       axios
       .get(url)
       .then(response=> 
@@ -22,15 +22,21 @@ function App() {
     <>
     <h1>Lista di attrici</h1>
 
+    <div className='row'>
     <ul className='list-unstyled'>
     {
       attrici.map(attrice => (
-        <li key={attrice.id}>
-          <img  src={attrice.image} />
+        <li key={attrice.id} className='col-6' >
+          <img  src={attrice.image} className='width-100' alt={attrice.name} />
+          <p>{attrice.name}</p>
+          <p>{attrice.birth_year}</p>
+          <p>{attrice.biography}</p>
+          <p>{attrice.awards}</p>
         </li>
       ))
     }
     </ul>
+    </div>
     </>
   )
 }
